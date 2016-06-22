1a3a1aaa5efb0e746e23d6cc7ee60b6ed34c4845
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-06-22 15:43:15 -0700

    added realm helperClass

---
+++ MakeSchoolNotes/Helpers/RealmHelper.swift
@@ -0,0 +1,40 @@
+//
+//  RealmHelper.swift
+//  MakeSchoolNotes
+//
+//  Created by Vyacheslav Horbach on 22/06/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmHelper {
+    static func addNote (note note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.add(note)
+        }
+    }
+    
+    static func deleteNote (note note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.delete(note)
+        }
+    }
+    
+    static func updatedNote(noteToBeUpdated: Note, newNote: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            noteToBeUpdated.title = newNote.title
+            noteToBeUpdated.content = newNote.content
+            noteToBeUpdated.modificationTime = newNote.modificationTime
+        }
+    }
+    
+    static func retrieveNotes(note note: Note) -> Results<Note> {
+        let realm = try! Realm()
+        return realm.objects(Note).sorted("modificationTime", ascending: false)
+    }
+}
