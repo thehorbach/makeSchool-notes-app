e677b7a7acd0e3395cd912ede5594b61767436f9
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-06-22 09:09:46 -0700

    downloaded provided files + started with TableViews

---
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        return cell
+    }
+  
+}
